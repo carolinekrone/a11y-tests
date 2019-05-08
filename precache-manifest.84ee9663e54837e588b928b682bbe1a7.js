fd7f836733f36b116e51da5cbaf51e4b00cc1f03
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-05-07 21:37:19 -0300

    Updates

---
+++ precache-manifest.84ee9663e54837e588b928b682bbe1a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc77b9c5464fa3305c027e9b114fe2b0",
+    "url": "/a11y-tests/index.html"
+  },
+  {
+    "revision": "a27e3ecd07670c886847",
+    "url": "/a11y-tests/static/js/2.a93c5699.chunk.js"
+  },
+  {
+    "revision": "354dfa511cdfa685b5e7",
+    "url": "/a11y-tests/static/js/main.c0c367b8.chunk.js"
+  },
+  {
+    "revision": "b2db8c57386a6ecfccde",
+    "url": "/a11y-tests/static/js/runtime~main.e03c795b.js"
+  },
+  {
+    "revision": "d6d91aa46a4bdb4698157e3166e1e52d",
+    "url": "/a11y-tests/static/media/acessibilidade.d6d91aa4.png"
+  },
+  {
+    "revision": "5460d49f71755054122402bc48027fd2",
+    "url": "/a11y-tests/static/media/default.5460d49f.png"
+  },
+  {
+    "revision": "27aa75205ee2047b40c765068b8a6f42",
+    "url": "/a11y-tests/static/media/logo-Ad-white.27aa7520.svg"
+  },
+  {
+    "revision": "05916d79f1e764f0ce110df65a4db64b",
+    "url": "/a11y-tests/static/media/logo-Ad.05916d79.svg"
+  },
+  {
+    "revision": "70d961018e8df1e049b4b03980bc8d13",
+    "url": "/a11y-tests/static/media/permanente.70d96101.png"
+  },
+  {
+    "revision": "d33e9429b6ce7b83d48a5c7d52884aac",
+    "url": "/a11y-tests/static/media/situacional.d33e9429.png"
+  },
+  {
+    "revision": "5c2378b3a67ce61a61e284bee24ecf8f",
+    "url": "/a11y-tests/static/media/temporaria.5c2378b3.png"
+  }
+]);
